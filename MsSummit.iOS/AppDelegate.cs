1d85977dc92180fa4d97899bbf6ce0f1dd77bfbb
Flavius Demian <flavius.demian@deventure.co>
2015-11-05 01:31:59 +0200

    updated AppDelegate

--- MsSummit.iOS/AppDelegate.cs
+++ MsSummit.iOS/AppDelegate.cs
@@ -10,6 +10,7 @@ namespace MsSummit.iOS
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
+    [Register("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
         UIWindow _window;
@@ -19,14 +20,14 @@ namespace MsSummit.iOS
 
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            _window = new UIWindow(UIScreen.MainScreen.Bounds);
             MainStoryBoardName = "MainStoryboard_iPhone";
             if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad)
             {
                 MainStoryBoardName = "MainStoryboard_iPad";
             }
             Storyboard = UIStoryboard.FromName(MainStoryBoardName, null);
-            initialViewController = Storyboard.InstantiateViewController("MainViewController");
-            initialViewController = initialViewController as MainViewController;
+            initialViewController = Storyboard.InstantiateInitialViewController();
             _window.RootViewController = initialViewController;
             _window.MakeKeyAndVisible();
 
--- MsSummit.iOS/MainViewController.cs
+++ MsSummit.iOS/MainViewController.cs
@@ -38,29 +38,26 @@ namespace MsSummit.iOS
         {
             base.ViewDidLoad();
 
-            var segment = new UISegmentedControl();
-            View.AddSubview(segment);
-
             // Perform any additional setup after loading the view, typically from a nib.
             #if __MOBILE__
-            Console.WriteLine("__MOBILE__ is defined");
+                        Console.WriteLine("__MOBILE__ is defined");
             #endif
 
             #if __IOS__
                         Console.WriteLine("__IOS__ is defined");
             #endif
 
-            //UILabel label1 = new UILabel();
-            //label1.Text = _mockService.RetrieveMockString();
-            //Add(label1);
+            UILabel label1 = new UILabel();
+            label1.Text = _mockService.RetrieveMockString();
+            Add(label1);
 
-            //UILabel label2 = new UILabel();
-            //label2.Text = _mockService.RetrievePartialStringWrapper();
-            //Add(label2);
+            UILabel label2 = new UILabel();
+            label2.Text = _mockService.RetrievePartialStringWrapper();
+            Add(label2);
 
-            //UILabel label3 = new UILabel();
-            //label1.Text = _mockService.RetrieveIFDEFString();
-            //Add(label3);
+            UILabel label3 = new UILabel();
+            label1.Text = _mockService.RetrieveIFDEFString();
+            Add(label3);
         }
     }
 }

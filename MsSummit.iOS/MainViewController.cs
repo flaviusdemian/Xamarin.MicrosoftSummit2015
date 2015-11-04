eaf07233fc7c6bed1f2fedb2fd780d9659d1ed5f
Flavius Demian <flavius.demian@deventure.co>
2015-11-05 01:41:01 +0200

    final commit

--- MsSummit.iOS/MainViewController.cs
+++ MsSummit.iOS/MainViewController.cs
@@ -39,25 +39,19 @@ namespace MsSummit.iOS
             base.ViewDidLoad();
 
             // Perform any additional setup after loading the view, typically from a nib.
-            #if __MOBILE__
-                        Console.WriteLine("__MOBILE__ is defined");
-            #endif
+#if __MOBILE__
+            Console.WriteLine("__MOBILE__ is defined");
+#endif
 
-            #if __IOS__
-                        Console.WriteLine("__IOS__ is defined");
-            #endif
+#if __IOS__
+            Console.WriteLine("__IOS__ is defined");
+#endif
 
-            UILabel label1 = new UILabel();
             label1.Text = _mockService.RetrieveMockString();
-            Add(label1);
 
-            UILabel label2 = new UILabel();
             label2.Text = _mockService.RetrievePartialStringWrapper();
-            Add(label2);
 
-            UILabel label3 = new UILabel();
-            label1.Text = _mockService.RetrieveIFDEFString();
-            Add(label3);
+            label3.Text = _mockService.RetrieveIFDEFString();
         }
     }
 }
--- MsSummit.iOS/MainViewController.designer.cs
+++ MsSummit.iOS/MainViewController.designer.cs
@@ -14,8 +14,32 @@ namespace MsSummit.iOS
 	[Register ("MainViewController")]
 	partial class MainViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel label1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel label2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel label3 { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (label1 != null) {
+				label1.Dispose ();
+				label1 = null;
+			}
+			if (label2 != null) {
+				label2.Dispose ();
+				label2 = null;
+			}
+			if (label3 != null) {
+				label3.Dispose ();
+				label3 = null;
+			}
 		}
 	}
 }
